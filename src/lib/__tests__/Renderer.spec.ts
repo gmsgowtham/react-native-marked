731264bf931a25ae19de83ef0137bb08d6c7603a
Gowtham G <webappsbygowtham@gmail.com>
2022-10-01 14:18:13 +0530

    test: renderer.getTextNode fn (snapshot, component)

---
+++ src/lib/__tests__/Renderer.spec.ts
@@ -0,0 +1,47 @@
+import type { ReactElement } from 'react';
+import { render, screen } from '@testing-library/react-native';
+import Renderer from '../Renderer';
+import getStyles from './../../theme/styles';
+
+const styles = getStyles();
+
+describe('Renderer', () => {
+  it('returns a <Text/> node', () => {
+    const renderer = new Renderer();
+    const TextNode = renderer.getTextNode(
+      'Hello world',
+      styles.text
+    ) as ReactElement;
+
+    const r = render(TextNode);
+    expect(screen.queryByText('Hello world')).toBeTruthy();
+    const tree = r.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('returns a wrapped <Text/> node', () => {
+    const renderer = new Renderer();
+    const TextNodeChild = renderer.getTextNode('Hello world', {});
+    const TextNode = renderer.getTextNode(
+      [TextNodeChild],
+      styles.text
+    ) as ReactElement;
+    const r = render(TextNode);
+    expect(screen.queryByText('Hello world')).toBeTruthy();
+    const tree = r.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('returns a wrapped <Text/> node with styles', () => {
+    const renderer = new Renderer();
+    const TextNodeChild = renderer.getTextNode('Hello world', styles.text);
+    const TextNode = renderer.getTextNode(
+      [TextNodeChild],
+      styles.text
+    ) as ReactElement;
+    const r = render(TextNode);
+    expect(screen.queryByText('Hello world')).toBeTruthy();
+    const tree = r.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
