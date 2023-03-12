7f0a746bb73f2ece1f08d63e104dd119d17d8e68
Gowtham G <webappsbygowtham@gmail.com>
2023-03-12 14:49:10 +0530

    refactor: impl private class features

--- src/lib/Parser.tsx
+++ src/lib/Parser.tsx
@@ -7,10 +7,11 @@ import { getValidURL } from "./../utils/url";
 import Renderer from "./Renderer";
 
 class Parser {
-	private renderer;
-	private styles: MarkedStyles;
-	private headingStylesMap: Record<number, TextStyle | undefined>;
-	private baseUrl: string;
+	#renderer: Renderer;
+	#styles: MarkedStyles;
+	#headingStylesMap: Record<number, TextStyle | undefined>;
+	#baseUrl: string;
+
 	constructor(options: ParserOptions) {
 		if (
 			typeof options.renderer === "undefined" ||
@@ -19,63 +20,67 @@ class Parser {
 			throw new Error("Renderer provided is not valid");
 		}
 
-		this.styles = { ...options.styles };
-		this.baseUrl = options.baseUrl ?? "";
-		this.renderer = options.renderer;
-		this.headingStylesMap = {
-			1: this.styles.h1,
-			2: this.styles.h2,
-			3: this.styles.h3,
-			4: this.styles.h4,
-			5: this.styles.h5,
-			6: this.styles.h6,
+		this.#styles = { ...options.styles };
+		this.#baseUrl = options.baseUrl ?? "";
+		this.#renderer = options.renderer;
+		this.#headingStylesMap = {
+			1: this.#styles.h1,
+			2: this.#styles.h2,
+			3: this.#styles.h3,
+			4: this.#styles.h4,
+			5: this.#styles.h5,
+			6: this.#styles.h6,
 		};
 	}
 
-	parse(tokens: marked.Token[]) {
-		return this._parse(tokens);
-	}
+	parse = (tokens: marked.Token[]) => {
+		return this.#parse(tokens);
+	};
 
-	private _parse(
+	#parse = (
 		tokens: marked.Token[],
 		styles?: ViewStyle | TextStyle | ImageStyle,
-	) {
+	) => {
 		const elements: ReactNode[] = tokens.map((token) => {
-			return this._parseToken(token, styles);
+			return this.#parseToken(token, styles);
 		});
 		return elements.filter((element) => element !== null);
-	}
+	};
 
-	private _parseToken(
+	#parseToken = (
 		token: marked.Token,
 		styles?: ViewStyle | TextStyle | ImageStyle,
-	): ReactNode {
+	): ReactNode => {
 		switch (token.type) {
 			case "space": {
 				return null;
 			}
 			case "paragraph": {
-				const children = this.getNormalizedSiblingNodesForBlockAndInlineTokens(
+				const children = this.#getNormalizedSiblingNodesForBlockAndInlineTokens(
 					token.tokens,
-					this.styles.text,
+					this.#styles.text,
 				);
 
-				return this.renderer.paragraph(children, this.styles.paragraph);
+				return this.#renderer.paragraph(children, this.#styles.paragraph);
 			}
 			case "blockquote": {
-				const children = this._parse(token.tokens);
-				return this.renderer.blockquote(children, this.styles.blockquote);
+				const children = this.#parse(token.tokens);
+				return this.#renderer.blockquote(children, this.#styles.blockquote);
 			}
 			case "heading": {
-				const styles = this.headingStylesMap[token.depth];
-				const children = this._parse(token.tokens, styles);
-				return this.renderer.heading(children, styles);
+				const styles = this.#headingStylesMap[token.depth];
+				const children = this.#parse(token.tokens, styles);
+				return this.#renderer.heading(children, styles);
 			}
 			case "code": {
-				return this.renderer.code(token.text, this.styles.code, this.styles.em);
+				return this.#renderer.code(
+					token.text,
+					this.#styles.code,
+					this.#styles.em,
+				);
 			}
 			case "hr": {
-				return this.renderer.hr(this.styles.hr);
+				return this.#renderer.hr(this.#styles.hr);
 			}
 			case "list": {
 				const li = token.items.map((item) => {
@@ -84,31 +89,31 @@ class Parser {
 							/* getViewNode since tokens could contain a block like elements (i.e. img) */
 							const childTokens = (cItem as marked.Tokens.Text).tokens || [];
 							const listChildren =
-								this.getNormalizedSiblingNodesForBlockAndInlineTokens(
+								this.#getNormalizedSiblingNodesForBlockAndInlineTokens(
 									childTokens,
-									this.styles.li,
+									this.#styles.li,
 								);
-							// return this.renderer.listItem(listChildren, this.styles.li);
+							// return this.#renderer.listItem(listChildren, this.#styles.li);
 							return listChildren;
 						}
 
 						/* Parse the nested token */
-						return this._parseToken(cItem);
+						return this.#parseToken(cItem);
 					});
 
-					return this.renderer.listItem(children, this.styles.li);
+					return this.#renderer.listItem(children, this.#styles.li);
 				});
 
-				return this.renderer.list(
+				return this.#renderer.list(
 					token.ordered,
 					li,
-					this.styles.list,
-					this.styles.li,
+					this.#styles.list,
+					this.#styles.li,
 				);
 			}
 			case "escape": {
-				return this.renderer.escape(token.text, {
-					...this.styles.text,
+				return this.#renderer.escape(token.text, {
+					...this.#styles.text,
 					...styles,
 				});
 			}
@@ -117,55 +122,55 @@ class Parser {
 				// in paragraph token, so will be handled via `getNormalizedSiblingNodesForBlockAndInlineTokens`
 				const linkStyle = {
 					...styles,
-					...this.styles.link, // To override color property
+					...this.#styles.link, // To override color property
 				};
-				const href = getValidURL(this.baseUrl, token.href);
-				const children = this._parse(token.tokens, linkStyle);
-				return this.renderer.link(children, href, linkStyle);
+				const href = getValidURL(this.#baseUrl, token.href);
+				const children = this.#parse(token.tokens, linkStyle);
+				return this.#renderer.link(children, href, linkStyle);
 			}
 			case "image": {
-				return this.renderer.image(
+				return this.#renderer.image(
 					token.href,
 					token.text || token.title,
-					this.styles.image,
+					this.#styles.image,
 				);
 			}
 			case "strong": {
 				const boldStyle = {
-					...this.styles.strong,
+					...this.#styles.strong,
 					...styles,
 				};
-				const children = this._parse(token.tokens, boldStyle);
-				return this.renderer.strong(children, boldStyle);
+				const children = this.#parse(token.tokens, boldStyle);
+				return this.#renderer.strong(children, boldStyle);
 			}
 			case "em": {
 				const italicStyle = {
-					...this.styles.em,
+					...this.#styles.em,
 					...styles,
 				};
-				const children = this._parse(token.tokens, italicStyle);
-				return this.renderer.em(children, italicStyle);
+				const children = this.#parse(token.tokens, italicStyle);
+				return this.#renderer.em(children, italicStyle);
 			}
 			case "codespan": {
-				return this.renderer.codespan(token.text, {
-					...this.styles.codespan,
+				return this.#renderer.codespan(token.text, {
+					...this.#styles.codespan,
 					...styles,
 				});
 			}
 			case "br": {
-				return this.renderer.br();
+				return this.#renderer.br();
 			}
 			case "del": {
 				const strikethroughStyle = {
-					...this.styles.strikethrough,
+					...this.#styles.strikethrough,
 					...styles,
 				};
-				const children = this._parse(token.tokens, strikethroughStyle);
-				return this.renderer.del(children, strikethroughStyle);
+				const children = this.#parse(token.tokens, strikethroughStyle);
+				return this.#renderer.del(children, strikethroughStyle);
 			}
 			case "text":
-				return this.renderer.text(token.raw, {
-					...this.styles.text,
+				return this.#renderer.text(token.raw, {
+					...this.#styles.text,
 					...styles,
 				});
 			case "html": {
@@ -174,8 +179,8 @@ class Parser {
 						"react-native-marked: rendering html from markdown is not supported",
 					);
 				}
-				return this.renderer.html(token.raw, {
-					...this.styles.text,
+				return this.#renderer.html(token.raw, {
+					...this.#styles.text,
 					...styles,
 				});
 			}
@@ -187,9 +192,9 @@ class Parser {
 				return null;
 			}
 		}
-	}
+	};
 
-	private getNormalizedSiblingNodesForBlockAndInlineTokens = (
+	#getNormalizedSiblingNodesForBlockAndInlineTokens = (
 		tokens: marked.Token[],
 		textStyle?: TextStyle,
 	): ReactNode[] => {
@@ -209,23 +214,23 @@ class Parser {
 					t.tokens[0].type === "image")
 			) {
 				// Render existing inline tokens in the queue
-				const parsed = this._parse(tokenRenderQueue);
+				const parsed = this.#parse(tokenRenderQueue);
 				if (parsed.length > 0) {
-					siblingNodes.push(this.renderer.text(parsed, textStyle));
+					siblingNodes.push(this.#renderer.text(parsed, textStyle));
 				}
 
 				// Render the current block token
 				if (t.type === "image") {
-					siblingNodes.push(this._parseToken(t));
+					siblingNodes.push(this.#parseToken(t));
 				} else if (t.type === "link") {
 					const imageToken = t.tokens[0] as marked.Tokens.Image;
-					const href = getValidURL(this.baseUrl, t.href);
+					const href = getValidURL(this.#baseUrl, t.href);
 					siblingNodes.push(
-						this.renderer.linkImage(
+						this.#renderer.linkImage(
 							href,
 							imageToken.href,
 							imageToken.text || imageToken.title,
-							this.styles.image,
+							this.#styles.image,
 						),
 					);
 				}
@@ -238,7 +243,7 @@ class Parser {
 
 		/* Remaining temp tokens if any */
 		if (tokenRenderQueue.length > 0) {
-			siblingNodes.push(this.renderer.text(this._parse(tokenRenderQueue), {}));
+			siblingNodes.push(this.#renderer.text(this.#parse(tokenRenderQueue), {}));
 		}
 
 		return siblingNodes;
