35d0ae34cf6776bad1453ce9e7fd7adc4681130e
Gowtham G <webappsbygowtham@gmail.com>
2023-03-22 19:23:57 +0530

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Markdown from "./lib/Markdown";
 import Renderer from "./lib/Renderer";
+import type { MarkdownProps, ParserOptions, IRenderer } from "./lib/types";
 
+export type { MarkdownProps, ParserOptions, IRenderer };
 export { Renderer };
 export default Markdown;
