cb87fda8f702e635460fcfd8d7206205fb6d6538
Gowtham G <webappsbygowtham@gmail.com>
2023-05-02 17:31:21 +0530

    feat(breaking): markdown hook support
    - Introduce useMarkdown hook for better extensibility
    - [breaking] Removed container style property

---
+++ src/hooks/useMarkdown.ts
@@ -0,0 +1,45 @@
+import { useMemo, type ReactNode } from "react";
+import { marked } from "marked";
+import type { MarkedStyles, UserTheme } from "./../theme/types";
+import Parser from "../lib/Parser";
+import Renderer from "../lib/Renderer";
+import getStyles from "./../theme/styles";
+import { type ColorSchemeName } from "react-native";
+import type { RendererInterface } from "../lib/types";
+
+export interface useMarkdownHookOptions {
+	colorScheme?: ColorSchemeName;
+	renderer?: RendererInterface;
+	theme?: UserTheme;
+	styles?: MarkedStyles;
+	baseUrl?: string;
+}
+
+const useMarkdown = (
+	value: string,
+	options?: useMarkdownHookOptions,
+): ReactNode[] => {
+	const styles = useMemo(
+		() => getStyles(options?.styles, options?.colorScheme, options?.theme),
+		[options?.styles, options?.theme, options?.colorScheme],
+	);
+
+	const parser = useMemo(
+		() =>
+			new Parser({
+				styles: styles,
+				baseUrl: options?.baseUrl,
+				renderer: options?.renderer ?? new Renderer(),
+			}),
+		[options?.renderer, options?.baseUrl, styles],
+	);
+
+	const elements = useMemo(() => {
+		const tokens = marked.lexer(value, { mangle: false, gfm: true });
+		return parser.parse(tokens);
+	}, [value, parser]);
+
+	return elements;
+};
+
+export default useMarkdown;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import Markdown from "./lib/Markdown";
 import Renderer from "./lib/Renderer";
+import useMarkdown, { type useMarkdownHookOptions } from "./hooks/useMarkdown";
 import type {
 	MarkdownProps,
 	ParserOptions,
 	RendererInterface,
 } from "./lib/types";
 
-export type { MarkdownProps, ParserOptions, RendererInterface };
-export { Renderer };
+export type {
+	MarkdownProps,
+	ParserOptions,
+	RendererInterface,
+	useMarkdownHookOptions,
+};
+export { Renderer, useMarkdown };
 export default Markdown;
