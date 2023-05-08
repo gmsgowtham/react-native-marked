e111d90abd65d0dabffe434526dc7d68da988412
Gowtham G <webappsbygowtham@gmail.com>
2023-05-08 18:27:05 +0530

    fix: remove fit image and implement custom

--- src/components/MDImage.tsx
+++ src/components/MDImage.tsx
@@ -1,6 +1,10 @@
-import React, { memo, useEffect } from "react";
-import { Image, type ImageStyle } from "react-native";
-import FitImage from "react-native-fit-image";
+import React, { memo, useEffect, useState } from "react";
+import {
+	Image,
+	ActivityIndicator,
+	type ImageStyle,
+	ImageBackground,
+} from "react-native";
 
 interface MDImageProps {
 	uri: string;
@@ -9,19 +13,39 @@ interface MDImageProps {
 }
 
 const MDImage = ({ uri, alt, style }: MDImageProps) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [aspectRatio, setAspectRatio] = useState<number>(0);
+
 	useEffect(() => {
 		Image.prefetch(uri);
-	}, [uri]);
+		fetchOriginalSizeFromRemoteImage();
+	}, []);
+
+	const onLoadEndOrOnError = () => {
+		if (isLoading) {
+			setIsLoading(false);
+		}
+	};
+
+	const fetchOriginalSizeFromRemoteImage = () => {
+		Image.getSize(uri, (width: number, height: number) => {
+			setAspectRatio(width / height);
+		});
+	};
 
 	return (
-		<FitImage
+		<ImageBackground
+			onLoadEnd={onLoadEndOrOnError}
+			onError={onLoadEndOrOnError}
+			source={{ uri: uri }}
+			style={[style, { width: "100%", aspectRatio }]}
+			resizeMode={"contain"}
 			accessibilityRole="image"
 			accessibilityLabel={alt || "Image"}
 			accessibilityHint={undefined}
-			source={{ uri }}
-			resizeMode="cover"
-			style={style}
-		/>
+		>
+			{isLoading ? <ActivityIndicator size={"small"} /> : null}
+		</ImageBackground>
 	);
 };
 
