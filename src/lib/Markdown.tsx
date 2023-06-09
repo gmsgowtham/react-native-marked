8d2ca87a55842485530056fc2ce5c95578667eee
yuhao1118 <liyuhao20001118@gmail.com>
2023-06-09 15:48:05 +0800

    fix: unique key

--- src/lib/Markdown.tsx
+++ src/lib/Markdown.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback } from "react";
+import React, { Fragment, memo, useCallback } from "react";
 import { FlatList, useColorScheme } from "react-native";
 import type { MarkdownProps, Token } from "./types";
 import { useMarkdownBase } from "../hooks/useMarkdown";
@@ -6,58 +6,63 @@ import colors from "../theme/colors";
 import type Parser from "./Parser";
 
 const MemoItem = memo(
-	({ item, parser }: { item: Token; parser: Parser }) => {
-		const elements = parser.parse([item]);
-		console.log("elements", elements);
-		return <>{elements}</>;
-	},
-	(prevProps, nextProps) => prevProps.item.raw === nextProps.item.raw,
+  ({ item, parser }: { item: Token; parser: Parser }) => {
+    const elements = parser.parse([item]);
+    return (
+      <>
+        {elements.map((item, index) => (
+          <Fragment key={index}>{item}</Fragment>
+        ))}
+      </>
+    );
+  },
+  (prevProps, nextProps) => prevProps.item.raw === nextProps.item.raw
 );
 
 const keyExtractor = (item: Token, index: number) => `${index}.${item.raw}`;
 
 const Markdown = ({
-	value,
-	flatListProps,
-	theme,
-	baseUrl,
-	renderer,
-	styles,
-	tokenizer,
+  value,
+  flatListProps,
+  theme,
+  baseUrl,
+  renderer,
+  styles,
+  tokenizer,
 }: MarkdownProps) => {
-	const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-	const { tokens, parser } = useMarkdownBase(value, {
-		theme,
-		baseUrl,
-		renderer,
-		colorScheme,
-		styles,
-		tokenizer,
-	});
+  const { tokens, parser } = useMarkdownBase(value, {
+    theme,
+    baseUrl,
+    renderer,
+    colorScheme,
+    styles,
+    tokenizer,
+  });
 
-	const renderItem = useCallback(
-		({ item }: { item: Token }) => <MemoItem item={item} parser={parser} />,
-		[parser],
-	);
+  const renderItem = useCallback(
+    ({ item }: { item: Token }) => <MemoItem item={item} parser={parser} />,
+    [parser]
+  );
 
-	return (
-		<FlatList
-			removeClippedSubviews={false}
-			keyExtractor={keyExtractor}
-			maxToRenderPerBatch={8}
-			initialNumToRender={8}
-			style={{
-				backgroundColor:
-					colorScheme === "light"
-						? colors.light.background
-						: colors.dark.background,
-			}}
-			{...flatListProps}
-			data={tokens}
-			renderItem={renderItem}
-		/>
-	);
+  return (
+    <FlatList
+      removeClippedSubviews={false}
+      keyExtractor={keyExtractor}
+      maxToRenderPerBatch={8}
+      initialNumToRender={8}
+      style={{
+        backgroundColor:
+          colorScheme === "light"
+            ? colors.light.background
+            : colors.dark.background,
+      }}
+      {...flatListProps}
+      data={tokens}
+      renderItem={renderItem}
+    />
+  );
 };
 
 export default memo(Markdown);
