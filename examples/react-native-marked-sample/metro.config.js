255aea963f908e3190d1de0e9fcd919bfd4f7098
Jacob Hummel <j4cobhummel@gmail.com>
2025-01-08 22:34:57 -0600

    fix: simplify metro config

--- examples/react-native-marked-sample/metro.config.js
+++ examples/react-native-marked-sample/metro.config.js
@@ -7,7 +7,7 @@ const pak = require("../../package.json");
 const root = path.resolve(__dirname, "../..");
 
 const modules = Object.keys({
-	...pak.peerDependencies,
+	...["react-native-web", ...pak.peerDependencies],
 });
 
 const defaultConfig = getDefaultConfig(__dirname);
@@ -32,18 +32,9 @@ module.exports = {
 			),
 		),
 
-		extraNodeModules: modules.reduce(
-			(acc, name) => {
-				acc[name] = path.join(__dirname, "node_modules", name);
-				return acc;
-			},
-			{
-				"react-native-web": path.join(
-					__dirname,
-					"node_modules",
-					"react-native-web",
-				),
-			},
-		),
+		extraNodeModules: modules.reduce((acc, name) => {
+			acc[name] = path.join(__dirname, "node_modules", name);
+			return acc;
+		}, {}),
 	},
 };
