f050abfb61b034acc4019bfbe434321896d970bf
Jacob Hummel <j4cobhummel@gmail.com>
2025-01-08 22:26:46 -0600

    fix: revert babel config + expo doctor

--- examples/react-native-marked-sample/babel.config.js
+++ examples/react-native-marked-sample/babel.config.js
@@ -1,6 +1,22 @@
+const path = require("node:path");
+const pak = require("../../package.json");
+
 module.exports = (api) => {
 	api.cache(true);
+
 	return {
 		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					extensions: [".tsx", ".ts", ".js", ".json"],
+					alias: {
+						// For development, we want to alias the library to the source
+						[pak.name]: path.join(__dirname, "..", "..", pak.source),
+					},
+				},
+			],
+		],
 	};
 };
